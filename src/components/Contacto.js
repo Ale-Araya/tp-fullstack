186f1390759f51ab533a83cdb10462d41c572453
Alejandro <ale_aleara@hotmail.com>
2023-02-24 23:26:03 -0300

    Nuevo proyecto

---
+++ src/components/Contacto.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Contacto = () => {
+    return (
+        <div>
+            <h2>Contactanos</h2>
+            <p>Telefono xxx-xxx-xxx</p>
+        </div>
+    );
+}
+
+export default Contacto;
---
+++ src/components/NavBar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href='/'>Inicio</a></li>
+                <li><a href='/Productos'>Productos</a></li>
+                <li><a href='/Contacto'>Contacto</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default NavBar;
